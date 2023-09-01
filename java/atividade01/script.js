a3f5b79add76d78cedba238d7eed036a45822b9d
unknown <gabrielsilva7988@gmail.com>
2023-09-01 11:41:13 -0300

    javascript

---
+++ java/atividade01/script.js
@@ -0,0 +1,16 @@
+const titulo = window.document.getElementsByTagName('h1')
+const divis = document.getElementsByClassName('quadrado')
+const redonda = document.getElementsByClassName('circulo')
+const text = document.getElementsByTagName('p')
+
+//querySelector - captura o primeiro que ele encontrar
+//querySelectorAll - captura todos 
+
+const lista = document.querySelectorAll('.quadrado')
+
+console.log(lista)
+
+// console.log(titulo)
+// console.log(divis)
+// console.log(redonda)
+// console.log(text)
